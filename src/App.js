1f1c935e051eb6b67059e4efcd4bb628fbf761a6
sobolev-ax <sobolev.ax@gmail.com>
2018-04-24 18:54:17 +0300

    p 130

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AddColorForm from './components/AddColorForm'
+
+class App extends Component {
+  render() {
+    return (
+      <AddColorForm />
+    );
+  }
+}
+
+export default App;
