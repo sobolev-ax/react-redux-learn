6d41e1f848cdc79ffa893e0a5f6d77d18092d664
sobolev-ax <sobolev.ax@gmail.com>
2018-06-06 16:49:39 +0300

    + fetch

--- chapter-seven/src/CountryList.js
+++ chapter-seven/src/CountryList.js
@@ -4,14 +4,35 @@ import fetch from 'isomorphic-fetch'
 class CountryList extends Component {
     constructor(props) {
         super(props)
-        
+        this.state = {
+            countryNames: [],
+            lading: false
+        }
     }
+
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch('https://restcountries.eu/rest/v1/all')
+            .then(response => response.json())
+            .then(json => json.map(country => country.name))
+            .then(countryNames =>
+                this.setState({countryNames, loading: false})
+            )
+    }
+
     render() {
-        return (
+        const { countryNames, loading } = this.state
+        return (loading) ?
             <section>
-                <h3>Fetch</h3>
-            </section>
-        )
+                <h3>CountryList loading..</h3>
+            </section> :
+            (!countryNames.length) ?
+                <h4>No country names</h4> :
+                <ul>
+                    {countryNames.map((x, i) =>
+                        <li key={i}>{x}</li>
+                    )}
+                </ul>
     }
 }
 
